26da6e1b52ccd7201b0b0457bd5bff4b6374e028
Albert <buboakstor@mail.ru>
2014-06-20 16:54:50 +0400

    1 лабараторная рабочая.

---
+++ MyLab/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace MyLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBarSearch = new System.Windows.Forms.ProgressBar();
+            this.progressBarReadFiles = new System.Windows.Forms.ProgressBar();
+            this.progressBarConvertation = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // progressBarSearch
+            // 
+            this.progressBarSearch.AccessibleDescription = "";
+            this.progressBarSearch.AccessibleName = "";
+            this.progressBarSearch.BackColor = System.Drawing.SystemColors.Control;
+            this.progressBarSearch.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.progressBarSearch.Location = new System.Drawing.Point(0, 154);
+            this.progressBarSearch.Name = "progressBarSearch";
+            this.progressBarSearch.Size = new System.Drawing.Size(100, 23);
+            this.progressBarSearch.Step = 1;
+            this.progressBarSearch.TabIndex = 1;
+            // 
+            // progressBarReadFiles
+            // 
+            this.progressBarReadFiles.Location = new System.Drawing.Point(0, 196);
+            this.progressBarReadFiles.Name = "progressBarReadFiles";
+            this.progressBarReadFiles.RightToLeftLayout = true;
+            this.progressBarReadFiles.Size = new System.Drawing.Size(100, 23);
+            this.progressBarReadFiles.TabIndex = 2;
+            // 
+            // progressBarConvertation
+            // 
+            this.progressBarConvertation.Location = new System.Drawing.Point(0, 238);
+            this.progressBarConvertation.Name = "progressBarConvertation";
+            this.progressBarConvertation.Size = new System.Drawing.Size(100, 23);
+            this.progressBarConvertation.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-3, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Поиск файлов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-3, 180);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Чтение файлов";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-3, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Конвертация";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(270, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 19);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Обзор";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(326, 0);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 261);
+            this.buttonConvert.TabIndex = 0;
+            this.buttonConvert.Text = "Обработать";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            this.backgroundWorker3.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker3_ProgressChanged);
+            this.backgroundWorker3.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker3_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBarConvertation);
+            this.Controls.Add(this.progressBarSearch);
+            this.Controls.Add(this.progressBarReadFiles);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBarReadFiles;
+        private System.Windows.Forms.ProgressBar progressBarConvertation;
+        private System.Windows.Forms.ProgressBar progressBarSearch;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonConvert;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+
+    }
+}
+
---
+++ MyLab/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Drawing.Imaging;
+
+
+
+namespace MyLab
+{
+    public partial class Form1 : Form
+    {
+        private string[] files_in_folder;
+        private List<string> JPEGFiles = new List<string>();
+        private List<Image> image = new List<Image>();
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+
+            backgroundWorker2.WorkerReportsProgress = true;
+            backgroundWorker2.WorkerSupportsCancellation = true;
+
+            backgroundWorker3.WorkerReportsProgress = true;
+            backgroundWorker3.WorkerSupportsCancellation = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((backgroundWorker1.IsBusy != true) || (backgroundWorker2.IsBusy != true) || (backgroundWorker3.IsBusy != true))
+            {
+                textBox1.Text = String.Empty;
+                folderBrowserDialog1.ShowDialog();
+                textBox1.AppendText(folderBrowserDialog1.SelectedPath);
+            }
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+            if ((backgroundWorker1.IsBusy != true) || (backgroundWorker2.IsBusy != true) || (backgroundWorker3.IsBusy != true))
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        //Поиск
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            Regex reg = new Regex(@"\.(?i:)(?:jpg|jpeg|JPG|JPEG)$");
+            
+
+            int files_count_jpgs_folder = 0;
+            files_in_folder = Directory.GetFiles(textBox1.Text, "*.*", SearchOption.AllDirectories);
+            files_count_jpgs_folder = files_in_folder.Length;
+
+            for (int i = 0; i < files_count_jpgs_folder; i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    int percentage = (i + 1) * 100 / files_count_jpgs_folder;
+                    if (reg.IsMatch(files_in_folder[i]))
+                    {
+                        JPEGFiles.Add(files_in_folder[i]);
+                    }   
+                    worker.ReportProgress(percentage);
+                }
+            }
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBarSearch.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (JPEGFiles.Count != 0)
+            {
+                backgroundWorker2.RunWorkerAsync();
+            }
+            else
+            {
+                MessageBox.Show("Нет jpg файлов в директории.");
+            }
+        }
+
+        //Чтение
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            int count_jpgs = JPEGFiles.Count;
+            for (int n = 0; n < count_jpgs; n++)
+            {
+                image.Add(Image.FromFile(JPEGFiles[n]));
+                int i = n;
+                //ImageStateObject state = new ImageStateObject();
+                //state.imageNum = n;
+                //state.path_of_file = JPEGFiles[i];
+                //FileStream fs = new FileStream(JPEGFiles[i], FileMode.Open, FileAccess.Read, FileShare.Read, 1, true);
+                //state.filestm = fs;
+                //int size = (int)fs.Length;
+                //byte[] data = new byte[size + 1];
+                //fs.BeginRead(data, 0, size,null,state);
+                //fs.BeginRead(data, 0, size, readImageCallback, state);
+                worker.ReportProgress((n + 1) * 100 / count_jpgs);
+            }
+        }
+
+        private void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBarReadFiles.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker2_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            backgroundWorker3.RunWorkerAsync();
+        }
+
+        //Обработка
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            int count_jpgs = image.Count;
+            for (int i = 0; i <= count_jpgs; i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    image[i].Save(Path.GetDirectoryName(JPEGFiles[i]) + @"\" + Path.GetFileNameWithoutExtension(JPEGFiles[i])+ ".png", ImageFormat.Png);
+                    image[i].Dispose();
+                    worker.ReportProgress((i + 1) * 100 / count_jpgs);
+                }
+            }
+        }
+
+        private void backgroundWorker3_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBarConvertation.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker3_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Обработка завершена");
+        }
+    }
+}
